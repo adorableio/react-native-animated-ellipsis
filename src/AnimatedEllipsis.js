d4e93efeee092ed9134d052e18e7d5d3657c6114
NickLewry <nicholas.lewry@icloud.com>
2020-08-14 08:37:32 +0100

    fix typo

--- src/AnimatedEllipsis.js
+++ src/AnimatedEllipsis.js
@@ -68,7 +68,7 @@ export default class AnimatedEllipsis extends Component {
     Animated.timing(this._animation_state.dot_opacities[which_dot], {
       toValue: this._animation_state.target_opacity,
       duration: this.props.animationDelay,
-      useNativeDriver: PropTypes.useNativeDriver,
+      useNativeDriver: this.props.useNativeDriver,
     }).start(this.animate_dots.bind(this, next_dot));
   }
 
